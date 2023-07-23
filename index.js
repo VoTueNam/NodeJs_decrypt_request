1ee6a2a229a52d2e1187f2a2d15ee900f3b26dc2
VoTueNam <tuenamvo@gmail.com>
2023-07-23 09:27:55 +0700

    change RSA padding

--- index.js
+++ index.js
@@ -18,36 +18,38 @@ const privateKey = fs.readFileSync("./private.key", "utf8");
 // Middleware to encrypt request data
 app.use(express.json());
 function encrypt(req, res, next) {
-  if (req.body) {
-    const requestData = JSON.stringify(req.body);
-    const encryptedData = crypto.publicEncrypt(
-      publicKey,
-      Buffer.from(requestData)
-    );
-    req.encryptedData = encryptedData.toString("base64");
-  }
-  return next();
-}
+  plaintext = req.body.plaintext;
+  const buffer = Buffer.from(plaintext, "utf8");
+  const encrypted = crypto.publicEncrypt(
+    { key: publicKey, padding: crypto.constants.RSA_PKCS1_PADDING },
+    buffer
+  );
+  req.body.plaintext = encrypted.toString("base64");
 
-// Middleware to decrypt request data
-function decrypt(req, res, next) {
-  if (req.body.encryptedData) {
-    const encryptedData = Buffer.from(req.body.encryptedData, "base64");
-    const decryptedData = crypto.privateDecrypt(privateKey, encryptedData);
-    req.decryptedData = JSON.parse(decryptedData.toString());
-  }
   return next();
 }
 
 // Route to handle the encrypted request
 app.post("/encrypt", encrypt, (req, res) => {
-  const requestData = req.encryptedData || req.body;
+  const requestData = req.body.plaintext;
   // Process the decrypted request data here...
   res.json({ "Your encrypt data": requestData });
 });
 
+// Middleware to decrypt request data
+function decrypt(req, res, next) {
+  ciphertext = req.body.ciphertext;
+  const buffer = Buffer.from(ciphertext, "base64");
+  const decrypted = crypto.privateDecrypt(
+    { key: privateKey, padding: crypto.constants.RSA_PKCS1_PADDING },
+    buffer
+  );
+  req.body.ciphertext = decrypted.toString("utf8");
+  return next();
+}
+
 app.post("/decrypt", decrypt, (req, res) => {
-  const requestData = req.decryptedData || req.body;
+  const requestData = req.body.ciphertext;
   // Process the decrypted request data here...
   res.json({ "Your plaintext data": requestData });
 });
